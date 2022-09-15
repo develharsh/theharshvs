7c1ef75a1534c64d119e1cb00515b21744de5177
unknown <hvsreal.223@gmail.com>
2022-09-15 15:32:50 +0530

    modified base url

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
   env: {
     // MONGODB_URL:
       // "mongodb+srv://hvs:thakur@cluster0.dqdk6.mongodb.net/production",
-    baseUrl: "http://localhost:3001",
-    // baseUrl: "https://gadifor.me",
+    // baseUrl: "http://localhost:3001",
+    baseUrl: "https://theharshvs.herokuapp.com",
   },
 };
